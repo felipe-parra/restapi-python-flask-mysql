89a666a9d9a84ea7c0cdde849b71638367f02a3c
felipe parra <felipe.parra@magnotechnology.com>
2021-10-12 08:20:37 -0500

    [update] response with a json data

--- src/app.py
+++ src/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, flash, json, jsonify
+from flask import Flask, request, jsonify
 from flask_mysqldb import MySQL
 from config import _MYSQL_HOST, _MYSQL_USER, _MYSQL_PASSWORD, _MYSQL_DB, _SECRET_KEY_APP
 
@@ -10,6 +10,7 @@ app.config["MYSQL_HOST"] = _MYSQL_HOST
 app.config["MYSQL_USER"] = _MYSQL_USER
 app.config["MYSQL_PASSWORD"] = _MYSQL_PASSWORD
 app.config["MYSQL_DB"] = _MYSQL_DB
+
 mysql = MySQL(app)
 
 # settings
@@ -17,26 +18,26 @@ app.secret_key = _SECRET_KEY_APP
 
 
 # routes
-@app.route('/clients', methods=["GET"])
+@app.route("/clients", methods=["GET"])
 def get_all_clients():
     """
     CLIENTS
-    GET ALL 
+    GET ALL
     """
     cursor = mysql.connection.cursor()
     cursor.execute("SELECT * FROM clients")
     data = cursor.fetchall()
     cursor.close()
-    message = {
-        "message": "clients listed",
-        "data":data
-        
-    }
+    data_res = []
+    for d in data:
+        data_res.append({"id": d[0], "name": d[1]})
+    message = {"message": "clients listed", "data": data_res}
     response = jsonify(message)
     response.status_code = 200
     return response
 
-@app.route('/clients/<id>', methods=["GET"])
+
+@app.route("/clients/<id>", methods=["GET"])
 def get_one_client(id):
     """
     CLIENTS
@@ -46,18 +47,16 @@ def get_one_client(id):
     cursor.execute("SELECT * FROM clients WHERE id = %s", (id))
     data = cursor.fetchall()
     cursor.close()
-    message = {
-        "message": "client listed",
-        "data":data
-        
-    }
+    data_res = {"id": data[0][0], "name": data[0][1]}
+    message = {"message": "client listed", "data": data_res}
     response = jsonify(message)
     response.status_code = 200
     return response
 
+
 # users
 # get all
-@app.route('/users', methods=["GET"])
+@app.route("/users", methods=["GET"])
 def get_all_users():
     """
     USERS
@@ -67,17 +66,17 @@ def get_all_users():
     cursor.execute("SELECT * FROM users")
     data = cursor.fetchall()
     cursor.close()
-    message = {
-        "message": "users listed",
-        "data":data
-        
-    }
+    data_res = []
+    for d in data:
+        data_res.append({"id": d[0], "name": d[1], "team": d[2]})
+    message = {"message": "users listed", "data": data_res}
     response = jsonify(message)
     response.status_code = 200
     return response
 
+
 # get one
-@app.route('/users/<id>', methods=["GET"])
+@app.route("/users/<id>", methods=["GET"])
 def get_one_user(id):
     """
     USERS
@@ -87,52 +86,52 @@ def get_one_user(id):
     cursor.execute("SELECT * FROM users WHERE id = %s", (id))
     data = cursor.fetchall()
     cursor.close()
-    message = {
-        "message": "user listed",
-        "data":data
-        
-    }
+    print(data)
+    data_res = {"id": data[0][0], "name": data[0][1], "teamId": data[0][2]}
+    message = {"message": "user listed", "data": data_res}
     response = jsonify(message)
     response.status_code = 200
     return response
 
+
 # get sales by user
-@app.route('/sales/users/<id>', methods=["GET"])
+@app.route("/sales/users/<id>", methods=["GET"])
 def get_sales_by_user(id):
     """
     sales
     GET by user
     """
     cursor = mysql.connection.cursor()
-    cursor.execute("SELECT u.name, SUM(s.amount) from sales as s, users as u where s.userId = u.id and u.id = %s group by u.id", (id))
+    cursor.execute(
+        "SELECT u.name, SUM(s.amount) from sales as s, users as u where s.userId = u.id and u.id = %s group by u.id",
+        (id),
+    )
     data = cursor.fetchall()
-    print(data["name"])
+
     cursor.close()
-    message = {
-        "message": "sales by user",
-        "data":data
-        
-    }
+    data_res = {"name": data[0][0], "sales": data[0][1]}
+    message = {"message": "sales by user", "data": data_res}
     response = jsonify(message)
     response.status_code = 200
     return response
 
+
 # get sales by teams
-@app.route('/sales/teams/<id>', methods=["GET"])
+@app.route("/sales/teams/<id>", methods=["GET"])
 def get_sales_by_teams(id):
     """
     sales
     GET by teams
     """
     cursor = mysql.connection.cursor()
-    cursor.execute("SELECT  t.name, SUM(s.amount) FROM sales AS s, users AS u, teams AS t WHERE s.userId = u.id AND u.teamId = t.id AND t.id = %s GROUP BY  t.id", (id))
+    cursor.execute(
+        "SELECT  t.name, SUM(s.amount) FROM sales AS s, users AS u, teams AS t WHERE s.userId = u.id AND u.teamId = t.id AND t.id = %s GROUP BY  t.id",
+        (id),
+    )
     data = cursor.fetchall()
     cursor.close()
-    message = {
-        "message": "sales by user",
-        "data":data
-        
-    }
+    data_res = {"name": data[0][0], "sales": data[0][1]}
+    message = {"message": "sales by teams", "data": data_res}
     response = jsonify(message)
     response.status_code = 200
     return response
@@ -140,14 +139,11 @@ def get_sales_by_teams(id):
 
 @app.errorhandler(404)
 def not_found(error=None):
-    message = {
-        "message": "Resource Not Found " + request.url,
-        "status": 404
-    }
+    message = {"message": "Resource Not Found " + request.url, "status": 404}
     response = jsonify(message)
     response.status_code = 404
     return response
 
 
-if __name__ == '__main__':
-    app.run(debug=False)
+if __name__ == "__main__":
+    app.run(debug=True)
